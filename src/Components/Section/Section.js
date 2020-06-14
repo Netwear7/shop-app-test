aa898f3da05ca013a25fbb079c35e0c251dabde0
Netwear7 <noel.julian-pro@outlook.fr>
2020-06-14 16:49:03 +0200

    save 14/06/2020 16:48

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -15,6 +15,8 @@ class Section extends Component{
         }
     }
 
+    //Pas de fonction fléchées car j'ai appelé les fonctions comme ça : {() => this.maFonction()}
+
     handleDeleteProduct(id){
         const array = this.state.products.slice();
         const index = array.findIndex(function(product){
@@ -50,72 +52,109 @@ class Section extends Component{
         const arrayProduct = this.state.products.slice();
         const product = arrayProduct[id]
         const countProductCart = this.state.countProductCart + 1
-        console.log(arrayProduct)
-        console.log(product)
+
         this.setState({
             countProductCart
         })
-
-
         if(this.checkProductNotInCart(product)){
             product.quantity = 1
             product.underTotal = product.quantity * product.price
-            
-            
+
             this.setState({
                 cart : [...this.state.cart, product],
                 total:  this.state.total + product.underTotal,
             })
-            
         }
         else{
             const arrayCart = this.state.cart.slice()
             const productInCart = arrayCart.find(element => element.id === id)
             const countProductCart = this.state.countProductCart + 1
+
             productInCart.quantity++
             productInCart.underTotal = productInCart.quantity * productInCart.price
+
             this.setState({
                 cart : arrayCart,
                 total:  this.state.total + productInCart.price,
                 countProductCart
             })
         }
-        console.log(this.state.countProductCart)
-        
-
-        // console.log(productWithQuantity)
-
     }
 
+    addQuantityCart(id){
+        const arrayCart = this.state.cart.slice()
+        const productInCart = arrayCart.find(element => element.id === id)
+        var countProductCart = this.state.countProductCart
+        const total = this.state.total + productInCart.price
+        
+        productInCart.quantity++;
+        countProductCart+=1;
+        productInCart.underTotal = productInCart.quantity * productInCart.price
+
+        this.setState({
+            cart: arrayCart,
+            countProductCart,
+            total
+        })
+
+    }
+    removeQuantityCart(id){
+        const arrayCart = this.state.cart.slice()
+        const productInCart = arrayCart.find(element => element.id === id)
+        var countProductCart = this.state.countProductCart
+
+        if(productInCart.quantity === 1){
+            return;
+        }
+        else if (productInCart.quantity >= 2){
+            var total = this.state.total - productInCart.price
+            productInCart.quantity--;
+            productInCart.underTotal = productInCart.quantity * productInCart.price
+            countProductCart-=1;
+        }
+        this.setState({
+            cart: arrayCart,
+            countProductCart,
+            total
+        })
+    }
+
+
+    
     render(){
         return (
             <div className="container-fluid bg-light">
                 <div className="row justify-content-end sticky-top">
-                    <div className="dropdown ml-auto sticky-top">
+                    <div className="dropdown ml-auto w-100 sticky-top">
+                        
                         <button 
-                            className="btn btn-light dropdown-toggle btn-cart position-button-cart sticky-top" 
+                            className="btn btn-light dropdown-toggle btn-cart position-button-cart sticky-top w-100" 
                             type="button" id="dropdownMenuButton" 
                             data-toggle="dropdown" 
-                            aria-haspopup="true" 
+                            aria-haspopup="true"    
                             aria-expanded="false">
-                            Panier ({this.state.countProductCart})
+                            <i className="fas fa-shopping-cart"></i> Panier ({this.state.countProductCart})
                         </button>
                         
-                        <div className="dropdown-menu dropdown-menu-right shopping-cart p-2" aria-labelledby="dropdownMenuButton">
+                        <div className="dropdown-menu dropdown-menu-right shopping-cart p-2 m-0 border-0" aria-labelledby="dropdownMenuButton">
                             <div className="row">
-                                <div className="col-6 offset-3">
-                                    <h3 className="mb-4">Liste d'achat(s)</h3>
+                                <div className="col-lg-6 offset-3">
+                                    <h3 className="mb-5 text-center">Liste d'achat(s)</h3>
                                 </div>
                             </div>
 
                             {
                             // vvvvvvvvvv opération ternaire sur plusieurs ligne vvvvvvvvvv
+
+                            //  réf :        https://medium.com/@robinpokorny/index-as-a-key-is-an-anti-pattern-e0349aece318
                             this.state.cart.length ?
                                 this.state.cart.map((cart, index) => (
                                     <ShoppingCart 
                                         cart={cart}
                                         clickHandlerDelete={() => this.handleDeleteProductCart(cart.id)}  
                                         key={index}
+                                        removeQuantity={() => this.removeQuantityCart(cart.id)}
+                                        addQuantity={() => this.addQuantityCart(cart.id)}
                                     />
                                 )) : 
                             (<div className="row m-3">
@@ -125,11 +164,13 @@ class Section extends Component{
                             </div>)
                             }
                             <div className="row px-3">
-                                <div className="col-2 ml-auto text-center text-black-50 m-0 p-0">
+                                <div className="col-lg-2 ml-auto text-center text-black-50 m-0 p-0">
                                     Total : 
                                 </div>
-                                <div className="col-2 text-center text-black-50 m-0 p-0">
+                                <div className="col-lg-2 text-center text-black-50 m-0 p-0">
                                     {this.state.total} €
+                                </div>
+                                <div className="col-lg-1 text-center text-black-50 m-0 p-0">
                                 </div>
                             </div>
                         </div>
--- src/Components/ShoppingCart/ShoppingCart.js
+++ src/Components/ShoppingCart/ShoppingCart.js
@@ -26,15 +26,39 @@ class ShoppingCart extends Component{
                         <span className="text-black-50"> {this.props.cart.marque}</span>
                     </div>
                     <div className="col-3 ml-auto border-left text-center">
-                        <span><i className="fas fa-chevron-left mr-1"></i></span>
-                        {this.props.cart.quantity} x {this.props.cart.price} 
-                        <span><i className="fas fa-chevron-right ml-1"></i></span>
+                        <div className="row">
+                            <div className="col-lg-2">
+                                <span>
+                                    <button 
+                                        onClick={this.props.removeQuantity}
+                                        className="p-0 border-0 bg-white">
+                                            <i className="fas fa-chevron-left"></i>
+                                    </button>
+                                </span>
+                            </div>
+
+                            <div className="col-lg-8">
+                                {this.props.cart.quantity} x {this.props.cart.price} 
+                            </div>
+                            
+                            <div className="col-lg-2">
+                                <span>
+                                    <button 
+                                        onClick={this.props.addQuantity}
+                                        className="p-0 border-0 bg-white">
+                                            <i className="fas fa-chevron-right"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+
+
                     </div>
                     <div className="col-2 text-center border-left">
                         {this.props.cart.underTotal} €
                         
                     </div>
-                    <div className="col-1 text-center ml-1 px-2 border-left">
+                    <div className="col-1 text-center ml-1 px-2 border-left d-flex justify-content-center">
                         <button 
                             type="button" 
                             className="close" 
